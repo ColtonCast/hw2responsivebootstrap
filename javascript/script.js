b3fd606ee49120b60f190ebb2723d7ba208b5866
ColtonCast <coltonjett400@gmail.com>
2018-10-11 18:15:34 -0700

    added portfolio links

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyADc6vSjUIpNwGQq2cLWrBoTSPTQ5aIdxc",
+    authDomain: "portfoliocontact-1980e.firebaseapp.com",
+    databaseURL: "https://portfoliocontact-1980e.firebaseio.com",
+    projectId: "portfoliocontact-1980e",
+    storageBucket: "",
+    messagingSenderId: "442096668527"
+  };
+  firebase.initializeApp(config);
